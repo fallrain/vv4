64e429854e75cb1acf9f66a024626623d1bd9167
王广开 <41666475@qq.com>
2019-05-10 15:23:37 +0800

    打包调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/require-prop-type-constructor': 0,
+    'comma-dangle': 0,//末尾逗号
+    'max-len': [1, 150],//最大行
+    'eqeqeq':1
   },
   parserOptions: {
     parser: 'babel-eslint',
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const env = process.env.NODE_ENV_MODE;
+module.exports = {
+  publicPath: env === 'prod' ? '/manage/' : '/',
+  devServer: {
+    host: '0.0.0.0',
+    port: 8093,
+    compress: true,
+    proxy: {
+      '/vipcenter': {
+        target: 'http://testvip.haier.net/',
+        changeOrigin: true
+      }
+    }
+  }
+};
